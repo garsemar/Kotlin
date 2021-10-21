5313951aa2db11331ce80d221bd8c61bdd037014
sjo <marti.garcia.7e4@itb.cat>
2021-10-21 12:33:20 +0200

    Mas ejercicios

---
+++ src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/uf1/data/DayOfWeek.kt
@@ -0,0 +1,19 @@
+package cat.itb.martigarcia7e4.dam.m03.uf1.data
+
+import java.util.*
+
+fun main(){
+    val scan = Scanner(System.`in`)
+    print("Introdueix un dia: ")
+    val dia = scan.nextInt()
+    val dias = mapOf(
+        1 to "Lunes",
+        2 to "Martes",
+        3 to "Mircoles",
+        4 to "Jueves",
+        5 to "Viernes",
+        6 to "Sabado",
+        7 to "Domingo"
+    )
+    print(dias[dia])
+}
---
+++ src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/uf1/data/IdentikitGenerator.kt
@@ -0,0 +1,10 @@
+package cat.itb.martigarcia7e4.dam.m03.uf1.data
+
+import java.util.*
+
+fun main(){
+    val scan = Scanner(System.`in`)
+    print("Introdueix un dia: ")
+    val dia = scan.nextInt()
+
+}
