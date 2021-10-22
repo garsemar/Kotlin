741669ad2a2672cb6dc4da7934e73cf6871857da
marti.garcia.7e4 <marti.garcia.7e4@itb.cat>
2021-10-22 11:39:44 +0200

    IsLeapYear

--- src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/uf1/seleccio/IsLeapYear.kt
+++ src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/uf1/seleccio/IsLeapYear.kt
@@ -7,7 +7,9 @@ fun main(){
     print("Introdueix un any: ")
     val year = scan.nextInt()
 
-    if(year % 4 == 0 || year % 400 == 0 && year % 100 == 0){
+    if(year % 100 == 0){
+        println("$year no és any de traspàs")
+    }else if(year % 4 == 0 || year % 400 == 0){
         println("$year és any de traspàs")
     }else{
         println("$year no és any de traspàs")
