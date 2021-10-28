f1b273074b802a2ccd0a57e0e4e2f9dafa54915d
sjo <marti.garcia.7e4@itb.cat>
2021-10-28 13:03:35 +0200

    Ex

---
+++ src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/uf1/iterative/HowManyLines.kt
@@ -0,0 +1,11 @@
+package cat.itb.martigarcia7e4.dam.m03.uf1.iterative
+
+import java.util.*
+
+fun main(){
+    val scan = Scanner(System.`in`)
+    print("Introdueix un numero: ")
+    var num = scan.nextInt()
+
+
+}
