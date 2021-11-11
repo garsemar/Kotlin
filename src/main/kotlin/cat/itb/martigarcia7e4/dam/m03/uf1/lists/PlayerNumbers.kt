7f451348088990f6fd18d48df0e86a8235b5e22a
sjo <sjo>
2021-11-11 12:56:49 +0100

    Ex

--- src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/uf1/lists/PlayerNumbers.kt
+++ src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/uf1/lists/PlayerNumbers.kt
@@ -2,12 +2,12 @@ package cat.itb.martigarcia7e4.dam.m03.uf1.lists
 
 import java.util.*
 
-fun main(){
+fun main() {
     val scan = Scanner(System.`in`)
     val listJ = mutableListOf<Int>()
     var a = 1
 
-    while(a <= 5){
+    while (a <= 5) {
         print("Introdueix un jugador: ")
         val pos = scan.nextInt()
         listJ.add(pos)
