b3d3af3ab8bb1945d7555c78b54576b8ffad2cab
garsemar <martigarciaserra@gmail.com>
2021-12-03 12:48:32 +0100

    ExamenLists

---
+++ src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/uf1/lists/exam/ListConfigurator.kt
@@ -0,0 +1,19 @@
+package cat.itb.martigarcia7e4.dam.m03.uf1.lists.exam
+
+import java.util.*
+
+fun main() {
+    val scan = Scanner(System.`in`)
+    val list = MutableList(10){0}
+    var pos = 0
+    var num = 0
+
+    while(true){
+        pos = scan.nextInt()
+        if(pos == -1) break
+        num = scan.nextInt()
+        if(num == -1) break
+        list.add(pos, num)
+    }
+    print(list)
+}
