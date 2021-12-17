6706b333564f1b0ffd597971b9b242d3e20df3d5
marti.garcia.7e4 <marti.garcia.7e4@itb.cat>
2021-12-17 12:59:35 +0100

    Nuls

---
+++ src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/uf1/nuls/LookForAnInt.kt
@@ -0,0 +1,22 @@
+package cat.itb.martigarcia7e4.dam.m03.uf1.nuls
+
+import java.util.*
+
+fun IntList(): MutableList<Int> {
+    val scan = Scanner(System.`in`)
+    print("Introdueix el numero d'elements de la llista: ")
+    var a = scan.nextInt()
+    val list = mutableListOf<Int>()
+    println("Introdueix els elements de la llista:")
+    while(a >= 1){
+        val num = scan.nextInt()
+        list.add(num)
+        a--
+    }
+    return list
+}
+
+fun main(){
+    val list = IntList()
+    print(list)
+}
