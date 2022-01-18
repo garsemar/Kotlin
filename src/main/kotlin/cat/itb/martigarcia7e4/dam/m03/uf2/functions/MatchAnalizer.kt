f6431a015e44e74ae148bba9ff1b5831bcbcc0aa
marti.garcia.7e4 <marti.garcia.7e4>
2022-01-18 13:11:12 +0100

    Functions

--- src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/uf2/functions/MatchAnalizer.kt
+++ src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/uf2/functions/MatchAnalizer.kt
@@ -2,50 +2,52 @@ package cat.itb.martigarcia7e4.dam.m03.uf2.functions
 
 import java.util.*
 
-data class teams(val nomEquip: String, val punts: MutableList<Int>)
+data class Teams(val nomEquip: String, val punts: MutableList<Int>)
 
 data class MatchAnalizer(var puntsA: Int, var puntsB: Int){
-    fun comprovar(listaA: List<Int>, listaB: List<Int>){
-        for(i in listaA.indices){
-            when (listaA[i]){
-                puntsA+1 -> tirL(listaA[i], nomEquipA, 0)
-                puntsA+2 -> cis(listaA[i], nomEquipA, 0)
-                puntsA+3 -> triple(listaA[i], nomEquipA, 0)
+    fun comprovar(listaA: Teams, listaB: Teams){
+        for(i in listaA.punts.indices){
+            when (listaA.punts[i]){
+                puntsA + 1 -> tirL(listaA.punts[i], listaA.nomEquip, 0)
+                puntsA + 2 -> cis(listaA.punts[i], listaA.nomEquip, 0)
+                puntsA + 3 -> triple(listaA.punts[i], listaA.nomEquip, 0)
             }
-        }
-        for(j in listaB.indices) {
-            when (listaB[j]) {
-                puntsB + 1 -> tirL(listaB[j], equipB, 1)
-                puntsB + 2 -> cis(listaB[j], equipB, 1)
-                puntsB + 3 -> triple(listaB[j], equipB, 1)
+            when (listaB.punts[i]) {
+                puntsB + 1 -> tirL(listaB.punts[i], listaB.nomEquip, 1)
+                puntsB + 2 -> cis(listaB.punts[i], listaB.nomEquip, 1)
+                puntsB + 3 -> triple(listaB.punts[i], listaB.nomEquip, 1)
             }
         }
     }
-
-    fun tirL(punts: Int, equip: String, ns: Int){
+    private fun tirL(punts: Int, equip: String, ns: Int){
         println("Tir lliure de $equip")
         if(ns == 0) puntsA = punts
         else puntsB = punts
     }
-    fun cis(punts: Int, equip: String, ns: Int){
+    private fun cis(punts: Int, equip: String, ns: Int){
         println("Cistella de $equip")
         if(ns == 0) puntsA = punts
         else puntsB = punts
     }
-    fun triple(punts: Int, equip: String, ns: Int){
+    private fun triple(punts: Int, equip: String, ns: Int){
         println("Triple de $equip")
         if(ns == 0) puntsA = punts
         else puntsB = punts
+    }
+    fun guanyador(teamA: Teams, teamB: Teams){
+        if(teamA.punts.last() > teamB.punts.last()){
+            println("Guanyar ${teamA.nomEquip}")
+        }else{
+            println("Guanyar ${teamB.nomEquip}")
+        }
     }
 }
 
 fun main(){
     val scan = Scanner(System.`in`)
     val matchAnalizer = MatchAnalizer(0, 0)
-    val nomEquipA = scan.next()
-    val nomEquipB = scan.next()
-    val teamA = teams(nomEquipA, mutableListOf())
-    val teamB = teams(nomEquipB, mutableListOf())
+    val teamA = Teams(scan.next(), mutableListOf())
+    val teamB = Teams(scan.next(), mutableListOf())
     var num: Int
     while (true){
         num = scan.nextInt()
@@ -57,5 +59,6 @@ fun main(){
             teamB.punts.add(num)
         } else break
     }
-    matchAnalizer.comprovar(teamA.punts, teamB.punts)
+    matchAnalizer.comprovar(teamA, teamB)
+    matchAnalizer.guanyador(teamA, teamB)
 }
