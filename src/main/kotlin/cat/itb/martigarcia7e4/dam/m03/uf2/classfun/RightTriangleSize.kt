05fb39b634559a1382928e13bf2acf9b63a81428
marti.garcia.7e4 <marti.garcia.7e4@itb.cat>
2022-01-21 13:10:38 +0100

    Functions generals

--- src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/uf2/classfun/RightTriangleSize.kt
+++ src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/uf2/classfun/RightTriangleSize.kt
@@ -1,2 +1,37 @@
 package cat.itb.martigarcia7e4.dam.m03.uf2.classfun
 
+import java.util.*
+import kotlin.math.sqrt
+
+data class RightTriangleSize(var base: MutableList<Double>, var altura: MutableList<Double>){
+    fun superficie(): MutableList<Double> {
+        val resu = mutableListOf<Double>()
+        for(i in 0..base.lastIndex){
+            resu.add((base[i]*altura[i])/2)
+        }
+        return resu
+    }
+    fun perimetre(): MutableList<Double> {
+        val resu = mutableListOf<Double>()
+        for(i in 0..base.lastIndex){
+            resu.add(sqrt((altura[i]*2)+(base[i]*2)))
+        }
+        return resu
+    }
+    fun toOutput(){
+        for(i in 0..base.lastIndex) {
+            println("Un triangle de ${base[i]} x ${altura[i]} té ${superficie()[i]} d'àrea i ${perimetre()[i]} de perímetre.")
+        }
+    }
+}
+
+fun main(){
+    val scan = Scanner(System.`in`)
+    val triangleSize = RightTriangleSize(mutableListOf(), mutableListOf())
+    val num = scan.nextInt()
+    repeat(num){
+        triangleSize.base.add(scan.nextDouble())
+        triangleSize.altura.add(scan.nextDouble())
+    }
+    triangleSize.toOutput()
+}
