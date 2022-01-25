5ffa6a6094b5d86dc6adfe2a873b68cc37e48b63
marti.garcia.7e4 <marti.garcia.7e4@itb.cat>
2022-01-25 12:15:29 +0100

    Functions generals

--- src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/uf2/classfun/RightTriangleSize.kt
+++ src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/uf2/classfun/RightTriangleSize.kt
@@ -4,14 +4,14 @@ import java.util.*
 import kotlin.math.sqrt
 
 data class RightTriangleSize(var base: MutableList<Double>, var altura: MutableList<Double>){
-    fun superficie(): MutableList<Double> {
+    private fun superficie(): MutableList<Double> {
         val resu = mutableListOf<Double>()
         for(i in 0..base.lastIndex){
             resu.add((base[i]*altura[i])/2)
         }
         return resu
     }
-    fun perimetre(): MutableList<Double> {
+    private fun perimetre(): MutableList<Double> {
         val resu = mutableListOf<Double>()
         for(i in 0..base.lastIndex){
             resu.add(sqrt((altura[i]*2)+(base[i]*2)))
@@ -20,7 +20,7 @@ data class RightTriangleSize(var base: MutableList<Double>, var altura: MutableL
     }
     fun toOutput(){
         for(i in 0..base.lastIndex) {
-            println("Un triangle de ${base[i]} x ${altura[i]} té ${superficie()[i]} d'àrea i ${perimetre()[i]} de perímetre.")
+            println("Un triangle de ${base[i]} x ${altura[i]} té ${"%.2f".format(superficie()[i])} d'àrea i ${"%.2f".format(perimetre()[i])} de perímetre.")
         }
     }
 }
