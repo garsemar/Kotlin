2c160f9e7fe8340a886785485b382656c3dddd02
marti.garcia.7e4 <marti.garcia.7e4@itb.cat>
2022-02-15 13:05:57 +0100

    Recursive

---
+++ src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/recursivity/MultiplicationRecursive.kt
@@ -0,0 +1,5 @@
+package cat.itb.martigarcia7e4.dam.m03.recursivity
+
+fun main() {
+
+}
---
+++ src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/recursivity/NPometes.kt
@@ -0,0 +1,24 @@
+package cat.itb.martigarcia7e4.dam.m03.recursivity
+
+fun getAppleSongStanza(applesCount: Int) =
+    """
+        $applesCount pometes té el pomer, 
+        de $applesCount una, de $applesCount una,
+        $applesCount pometes té el pomer,
+        de $applesCount una en caigué. 
+        Si mireu el vent d'on vé 
+        veureu el pomer com dansa, 
+        si mireu el vent d'on vé
+        veureu com dansa el pomer.
+    """
+
+fun getAppleSong(applesCount: Int):String{
+    if(applesCount == 0){
+        return ""
+    }
+    return getAppleSongStanza(applesCount) + getAppleSong(applesCount-1)
+}
+
+fun main() {
+    print(getAppleSong(10))
+}
