b3cb691289001f6f21cf7ffe196e14c48181f677
marti.garcia.7e4 <marti.garcia.7e4@itb.cat>
2022-02-17 11:18:45 +0100

    Recursive

--- src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/recursivity/MultiplicationRecursive.kt
+++ src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/recursivity/MultiplicationRecursive.kt
@@ -1,5 +1,15 @@
 package cat.itb.martigarcia7e4.dam.m03.recursivity
 
-fun main() {
+import java.util.*
 
+fun mult(num1: Int, num2: Int){
+
+}
+
+fun main(){
+    val scan = Scanner(System.`in`)
+    val num = scan.nextInt()
+    val num2 = scan.nextInt()
+
+    println(num*num2)
 }
