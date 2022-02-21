809c18e896c52184bfb419d82ead8d2a2c42c6db
garsemar <martigarciaserra@gmail.com>
2022-02-21 20:44:33 +0100

    Recursive

--- src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/recursivity/MultiplicationRecursive.kt
+++ src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/recursivity/MultiplicationRecursive.kt
@@ -2,8 +2,11 @@ package cat.itb.martigarcia7e4.dam.m03.recursivity
 
 import java.util.*
 
-fun mult(num1: Int, num2: Int){
-
+fun mult(num: Int, num2: Int): Int{
+    if(num2 == 0){
+        return 0
+    }
+    return num + mult(num, num2-1)
 }
 
 fun main(){
@@ -11,5 +14,5 @@ fun main(){
     val num = scan.nextInt()
     val num2 = scan.nextInt()
 
-    println(num*num2)
+    println(mult(num, num2))
 }
---
+++ src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/recursivity/Power.kt
@@ -0,0 +1,10 @@
+package cat.itb.martigarcia7e4.dam.m03.recursivity
+
+import java.util.*
+
+fun main() {
+    val scan = Scanner(System.`in`)
+    val num = scan.nextInt()
+
+    println(point(num))
+}
