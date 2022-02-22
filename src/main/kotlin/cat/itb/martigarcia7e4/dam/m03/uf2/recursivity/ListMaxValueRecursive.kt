23a2ffa8ccf8b412361b6d2759bcb4fe81cfc177
marti.garcia.7e4 <marti.garcia.7e4@itb.cat>
2022-02-22 13:21:39 +0100

    Recursive

--- src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/uf2/recursivity/ListMaxValueRecursive.kt
+++ src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/uf2/recursivity/ListMaxValueRecursive.kt
@@ -2,12 +2,29 @@ package cat.itb.martigarcia7e4.dam.m03.uf2.recursivity
 
 import java.util.*
 
-/*fun listM(num: Int): Int{
-
-}*/
+fun listM(list: MutableList<Int>): Int{
+    if(list.isEmpty()){
+        return 0
+    }
+    if(list.indices == 0..0){
+        return list[0]
+    }
+    if(list[0] > list[1]){
+        list.removeAt(1)
+    }
+    else if(list[0] < list[1]){
+        list.removeAt(0)
+    }
+    return listM(list)
+}
 
 fun main() {
     val scan = Scanner(System.`in`)
-    val num = scan.nextInt()
-    println(fact(num))
+    val list = mutableListOf<Int>()
+    while(true){
+        val num = scan.nextInt()
+        if(num == -1) break
+        list.add(num)
+    }
+    println(listM(list))
 }
