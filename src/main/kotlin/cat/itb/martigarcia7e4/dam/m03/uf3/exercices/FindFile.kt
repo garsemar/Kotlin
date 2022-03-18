f33cb221c942141e48cb6ba7bdeb4afadd88885d
sjo <awd>
2022-03-18 12:55:04 +0100

    Files

--- src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/uf3/exercices/FindFile.kt
+++ src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/uf3/exercices/FindFile.kt
@@ -1,16 +1,9 @@
 package cat.itb.martigarcia7e4.dam.m03.uf3.exercices
 
-import java.io.File
-import java.nio.file.Path
 import java.util.*
-import kotlin.io.path.listDirectoryEntries
 
 fun main() {
     val scan = Scanner(System.`in`)
-    val path: Path = Path.of(scan.next())
-    val file: File = File(scan.next())
-
-    val files: List<Path> = path.listDirectoryEntries()
-
-    files
+    val file = scan.next()
+    
 }
