4dc334450c9552540791f4dcbc97cb556e06a176
sjo <awd>
2022-03-25 12:56:35 +0100

    UF4

--- src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/uf4/exercices/intro/MechanicalArmApp.kt
+++ src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/uf4/exercices/intro/MechanicalArmApp.kt
@@ -1,27 +1,44 @@
 package cat.itb.martigarcia7e4.dam.m03.uf4.exercices.intro
 
-class MechanicalArm(){
-    private var status: Boolean = false
-    private var angle: Int = 0
-    private var altitude: Int = 0
-
-    fun setTurnedOn(status: Boolean){
-        this.status = status
+data class MechanicalArm(
+    var status: Boolean = false,
+    var angle: Int = 0,
+    var altitude: Int = 0
+){
+    fun toggle(): Boolean {
+        status = !status
+        println(this)
+        return status
     }
 
-    fun updateAltitude(altitude: Int){
+    fun updateAltitude(altitude: Int): Int {
         if(status) {
             this.altitude += altitude
+            println(this)
+            return altitude
         }
+        println(this)
+        return altitude
     }
 
-    fun updateAngle(angle: Int){
+    fun updateAngle(angle: Int): Int {
         if(status) {
             this.angle += angle
+            println(this)
+            return angle
         }
+        println(this)
+        return angle
     }
 }
 
 fun main() {
-
+    val mechanicalArm = MechanicalArm()
+    mechanicalArm.toggle()
+    mechanicalArm.updateAltitude(3)
+    mechanicalArm.updateAngle(180)
+    mechanicalArm.updateAltitude(-3)
+    mechanicalArm.updateAngle(-180)
+    mechanicalArm.updateAltitude(3)
+    mechanicalArm.toggle()
 }
