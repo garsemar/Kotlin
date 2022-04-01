5bf6ce3846407fd5e2d69d1c47ef637363759f93
garsemar <martigarciaserra@gmail.com>
2022-04-01 12:58:54 +0200

    Enums

---
+++ src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/uf4/exercices/enums/Rainbow.kt
@@ -0,0 +1,23 @@
+package cat.itb.martigarcia7e4.dam.m03.uf4.exercices.enums
+
+import java.util.*
+
+enum class Rainbow{
+    ROJO, NARANJA, AMARILLO, VERDE, AZUL, VIOLETA
+}
+
+fun main() {
+    val scan = Scanner(System.`in`)
+    val input = scan.next()
+    var a = false
+    for(i in Rainbow.values()){
+        if(input.equals(i.toString())){
+            a = true
+            break
+        }
+        else{
+            a = false
+        }
+    }
+    println(a)
+}
