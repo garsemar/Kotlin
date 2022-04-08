6b0983de31e1e50839895df48583bde42e7fb731
garsemar <martigarciaserra@gmail.com>
2022-04-08 13:02:02 +0200

    Interfaces

---
+++ src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/uf4/exercices/inter/VehicleComparableApp.kt
@@ -0,0 +1,43 @@
+package cat.itb.martigarcia7e4.dam.m03.uf4.exercices.inter
+
+interface VehicleShop{
+    val bicycleList: MutableList<BicycleModel>
+    val scooterList: MutableList<ScooterModel>
+}
+
+data class VehicleBrand(val name: String, val country: String)
+
+open class Model(val name: String, val brand: VehicleBrand)
+
+class BicycleModel(name: String, val gears: Int, brand: VehicleBrand): Model(name, brand){
+    override fun toString(): String {
+        return "BicycleModel{name=$name, gears=$gears, brand=VehicleBrand{name=${brand.name}, country=${brand.country}}}"
+    }
+}
+
+class ScooterModel(name: String, val power: Double, brand: VehicleBrand): Model(name, brand){
+    override fun toString(): String {
+        return "ScooterModel{name=$name, power=$power, brand=VehicleBrand{name=${brand.name}, country=${brand.country}}}"
+    }
+}
+
+fun main() {
+    val bicycleList = mutableListOf(
+        BicycleModel("modelX", 5, VehicleBrand("lorem", "Togo")),
+        BicycleModel("modelZ", 2, VehicleBrand("lorem", "Togo")),
+        BicycleModel("modelY", 6, VehicleBrand("lorem", "Togo"))
+    )
+    val scooterList = mutableListOf(
+        ScooterModel("s562", 45.3, VehicleBrand("lorem", "Togo")),
+        ScooterModel("y463", 25.2, VehicleBrand("lorem", "Togo")),
+        ScooterModel("s672", 85.8, VehicleBrand("lorem", "Togo"))
+    )
+
+    bicycleList.forEach { println(it) }
+    scooterList.forEach { println(it) }
+    println("-------------------")
+    bicycleList.sortBy { it.name }
+    scooterList.sortBy { it.name }
+    bicycleList.forEach { println(it) }
+    scooterList.forEach { println(it) }
+}
--- src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/uf4/exercices/inter/bcntrees/BcnTrees.kt
+++ src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/uf4/exercices/inter/bcntrees/BcnTrees.kt
@@ -6,25 +6,50 @@ import io.ktor.client.features.json.*
 import io.ktor.client.features.json.serializer.*
 import io.ktor.client.request.*
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 import java.util.*
+import kotlin.io.path.Path
+import kotlin.io.path.readText
+
+interface BcnTreesDataSource{
+    suspend fun listTrees(): List<BcnTree>
+}
+
+class APIBcnTreesDataSource(): BcnTreesDataSource{
+    override suspend fun listTrees(): List<BcnTree> {
+        val client = HttpClient(CIO) {
+            install(JsonFeature) {
+                serializer = KotlinxSerializer(kotlinx.serialization.json.Json {
+                    ignoreUnknownKeys = true
+                })
+            }
+        }
+        return client.get("https://opendata-ajuntament.barcelona.cat/resources/bcn/Arbrat/OD_Arbrat_Zona_BCN.json")
+    }
+}
+class LocalBcnTreesDataSource(): BcnTreesDataSource{
+    private val json = Json { ignoreUnknownKeys = true }
+
+    override suspend fun listTrees(): List<BcnTree> {
+        val path = Path("src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/uf4/resources/OD_Arbrat_Zona_BCN.json")
+
+        return json.decodeFromString(path.readText())
+    }
+}
 
 @Serializable
 data class BcnTree(val nom_cientific: String)
 
 suspend fun main() {
     val scan = Scanner(System.`in`)
+    val jsonType = scan.nextLine()
     val typeTree = scan.nextLine()
 
-    val client = HttpClient(CIO) {
-        install(JsonFeature) {
-            serializer = KotlinxSerializer(kotlinx.serialization.json.Json {
-                ignoreUnknownKeys = true
-            })
-        }
-    }
+    val useApi: Boolean = jsonType != "LOCAL"
+    val bcnTreeDataSource : BcnTreesDataSource = if(useApi) APIBcnTreesDataSource() else LocalBcnTreesDataSource()
+    val trees = bcnTreeDataSource.listTrees()
 
-    val trees: List<BcnTree> =
-        client.get("https://centellas.gitlab.io/teachingSite/DAM-M03/UF3/exercicis/files/OD_Arbrat_Zona_BCN.json")
     var count = 0
     for(tree in trees){
         if(tree.nom_cientific == typeTree){
