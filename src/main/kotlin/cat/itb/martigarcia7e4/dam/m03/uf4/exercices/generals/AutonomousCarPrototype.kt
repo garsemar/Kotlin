f4f680087009a08a193dcad19fe5230a99e32bc0
garsemar <martigarciaserra@gmail.com>
2022-05-03 00:07:04 +0200

    Exam

---
+++ src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/uf4/exercices/generals/AutonomousCarPrototype.kt
@@ -0,0 +1,38 @@
+package cat.itb.martigarcia7e4.dam.m03.uf4.exercices.generals
+
+enum class Direction{
+    FRONT, LEFT, RIGHT
+}
+
+interface CarSensors{
+    fun isThereSomethingAt(direction: Direction) : Boolean
+    fun go(direction : Direction)
+    fun stop()
+}
+
+class AutonomousCar(private val carSensors: CarSensors){
+    fun doNextNSteps(n :Int){
+        repeat(n){
+            nextStep()
+        }
+    }
+
+    private fun isPosibleGo(direction: Direction): Boolean{
+        if(carSensors.isThereSomethingAt(direction)){
+            return false
+        }
+        return true
+    }
+    fun nextStep(){
+        if(isPosibleGo(Direction.FRONT)){
+            carSensors.go(Direction.FRONT)
+        }
+        else if(isPosibleGo(Direction.RIGHT)){
+            carSensors.go(Direction.RIGHT)
+        }
+        else if(isPosibleGo(Direction.LEFT)){
+            carSensors.go(Direction.LEFT)
+        }
+        carSensors.stop()
+    }
+}
