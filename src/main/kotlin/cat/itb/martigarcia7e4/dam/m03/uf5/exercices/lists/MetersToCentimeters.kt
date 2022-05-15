c62366c7178ee9af4b4fa3a5284ca7f71b7d8308
garsemar <martigarciaserra@gmail.com>
2022-05-15 14:26:43 +0200

    CenToMet

--- src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/uf5/exercices/lists/MetersToCentimeters.kt
+++ src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/uf5/exercices/lists/MetersToCentimeters.kt
@@ -1,7 +1,14 @@
 package cat.itb.martigarcia7e4.dam.m03.uf5.exercices.lists
 
-import cat.itb.martigarcia7e4.dam.m03.uf2.functions.IntegerLists
+import java.util.*
 
 fun main() {
+    val scanner = Scanner(System.`in`)
+    val meters = mutableListOf<Int>()
+    repeat(scanner.nextInt()) {
+        meters.add(scanner.nextInt())
+    }
+    val centimeters = meters.map { it*100 }
 
+    centimeters.forEach { println(it) }
 }
--- src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/uf5/exercices/tads/maps/RoadSigns.kt
+++ src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/uf5/exercices/tads/maps/RoadSigns.kt
@@ -1,5 +1,9 @@
 package cat.itb.martigarcia7e4.dam.m03.uf5.exercices.tads.maps
 
-fun main() {
+fun main(){
+    // fes una llista d'string
+    val list = listOf("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
 
+    // printa hola utilitzant la llista
+    println("Hola ${list[0]}")
 }
