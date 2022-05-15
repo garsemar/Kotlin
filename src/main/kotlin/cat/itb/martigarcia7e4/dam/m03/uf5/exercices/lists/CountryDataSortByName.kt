46f68063d28de2e3a8ff08f44ad37288eaa0b284
garsemar <martigarciaserra@gmail.com>
2022-05-15 20:58:13 +0200

    Functional

---
+++ src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/uf5/exercices/lists/CountryDataSortByName.kt
@@ -0,0 +1,19 @@
+package cat.itb.martigarcia7e4.dam.m03.uf5.exercices.lists
+
+import java.util.*
+
+data class Country(var nom: String, var capital: String, val superficie: Int, val densitat: Int){
+
+}
+
+fun main() {
+    val scan = Scanner(System.`in`)
+
+    val countryList = ArrayList<Country>()
+    repeat(scan.nextInt()) {
+        countryList.add(Country(scan.next(), scan.next(), scan.nextInt(), scan.nextInt()))
+    }
+
+    countryList.sortBy { it.nom }
+    countryList.forEach { if(it.densitat > 5 && it.superficie > 1000) println(it.nom) }
+}
