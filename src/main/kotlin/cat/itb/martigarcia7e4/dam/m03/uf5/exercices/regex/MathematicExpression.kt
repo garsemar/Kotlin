201b4d48f2c9932476cd7f6a8831130a8c82027b
El vostre nom <usuari@domini.com>
2022-05-24 13:18:32 +0200

    Exam UF5

--- src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/uf5/exercices/regex/MathematicExpression.kt
+++ src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/uf5/exercices/regex/MathematicExpression.kt
@@ -1,2 +1,17 @@
 package cat.itb.martigarcia7e4.dam.m03.uf5.exercices.regex
 
+fun main() {
+    val input = "25+3(1+2+(304))/(2"
+
+    val regexs = listOf(Regex("[()]"), Regex("[(]..[)]|[(][)]"))
+
+    val matches = makeReadable(regexs[0].findAll(input))
+    val res2 = makeReadable(regexs[1].findAll(matches))
+
+    if(res2.length == matches.length) println("Correct")
+    else println("Incorrect")
+}
+
+fun makeReadable(input: Sequence<MatchResult>): String {
+    return input.map { it.value }.joinToString("")
+}
--- src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/uf5/exercices/regex/Mine.kt
+++ src/main/kotlin/cat/itb/martigarcia7e4/dam/m03/uf5/exercices/regex/Mine.kt
@@ -7,31 +7,21 @@ data class Stats(var white: Int, var black: Int)
 fun main() {
     val scan = Scanner(System.`in`)
 
-    val text = "<><<>><(<)"
+    val text = "(())<><()>"
     val combos = Regex("[(][)]|<>|<..>|[(]..[)]")
-    val matches = combos.findAll(text)
-    val names: Sequence<List<String>> = matches.map { it.groupValues }
+    val names = combos.findAll(text).map { it.value }
 
     val stats = Stats(0, 0)
-    for(i in names){
-        var comb = i.toString()
-        comb = comb.dropLast(1)
-        comb = comb.substring(1)
-        if(comb == "<>"){
-            stats.white += 1
-        }
-        else if(comb == "()"){
-            stats.black += 1
-        }
-        else if(comb == "<<>>"){
-            stats.white += 2
-        }
-        else if(comb == "(())"){
-            stats.black += 2
-        }
-        else if(comb == "<()>" || comb == "(<>)"){
-            stats.white += 1
-            stats.black += 1
+    names.forEach {
+        when(it){
+            "<>" -> stats.white++
+            "()" -> stats.black++
+            "<<>>" -> stats.white += 2
+            "(())" -> stats.black += 2
+            "<()>", "(<>)" -> {
+                stats.white += 1
+                stats.black += 1
+            }
         }
     }
     println("${stats.white}b ${stats.black}n")
