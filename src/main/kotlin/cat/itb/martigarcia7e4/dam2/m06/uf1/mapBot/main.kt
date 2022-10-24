ebcc4775b25ac94db0ca64ea8d693c7c1e0c3c6d
«garsemar» <martigarciaserra@gmail.com>
2022-10-24 16:50:56 +0200

    Move MapBot

--- src/main/kotlin/cat/itb/martigarcia7e4/dam2/m06/uf1/mapBot/main.kt
+++
@@ -1,9 +0,0 @@
-package cat.itb.martigarcia7e4.dam2.m06.uf1.mapBot
-
-import cat.itb.martigarcia7e4.dam2.m06.uf1.mapBot.ui.UI
-
-fun main() {
-    val ui = UI()
-
-    ui.runBot()
-}
--- src/main/kotlin/cat/itb/martigarcia7e4/dam2/m06/uf1/mapBot/model/tmbRoute/From.kt
+++
@@ -1,9 +0,0 @@
-package cat.itb.martigarcia7e4.dam2.m06.uf1.mapBot.model.tmbRoute
-
-import com.google.gson.annotations.SerializedName
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class From(
-    @SerializedName("name") var name: String? = null
-)
